d1ffe5c66cb932c90099116dbf1b2f92ae213af3
郝小龙 <1337970681@qq.com>
2019-02-16 18:59:23 +0800

    实现动态首页   郝小龙

--- js/index.js
+++ js/index.js
@@ -201,6 +201,7 @@
     };
 
 
+
     // 公司
     var company_main=document.querySelector(".company_main");
     getData('http://127.0.0.1:8080/api/company/indexCompanyList/',null,null,function (res) {
@@ -235,24 +236,114 @@
 
 
 
+    // 日记
+    var user_icon02=document.querySelector(".user_icon02");
+    getData('http://127.0.0.1:8080/api/diary/diaryUserIcon/',null,null,function (res){
+        if(res && res['status_code']==='10009'){
+            for(let r of res['content']){
+                user_icon02.innerHTML+=`<div class="col-xs-12 user_item" id="${r["id"]}">
+                            <img src="${r["icon"]}" alt="">
+                        </div>`
+            }
 
 
+            var user_items=document.querySelectorAll(".user_item");
+            var first_diary_id=user_items[0].id;
+            function getDiaryData(args) {
+                getData("http://127.0.0.1:8080/api/diary/diaryItem/",{"diary_id":args},null,function (res) {
+                    if (res && res['status_code']==='10009') {
+                        var diary_text = document.querySelector(".diary_text");
+                        var diary_title = document.querySelector(".diary_title span");
+                        document.querySelector(".user_icon01 img").src=res["content"]["icon"];
+                        diary_title.innerText=res["content"]["diary_title"];
+                        document.querySelector(".style").innerText=res["content"]["style_name"];
+                        document.querySelector(".diary_company_name").innerText=res["content"]["company"];
+                        diary_text.innerText=res["content"]["diary_content"];
+                        var imgs="";
+                        for (var img of res["content"]["diary_img"]){
+                            imgs +=`<img src="${img}">`
+                        }
+                        document.querySelector(".diary_image").innerHTML = imgs;
 
-//字数限制{
-    var diary_text = document.querySelector(".diary_text");
-    var diary_title = document.querySelector(".diary_title span");
-    var strategy_content_text=document.querySelectorAll(".strategy_content_text a");
-    if (diary_text.innerText.length > 60) {
-        diary_text.innerText = diary_text.innerText.substring(0, 60) + "..."
-    }
-    if (diary_title.innerText.length > 15) {
-        diary_title.innerText = diary_title.innerText.substring(0, 15) + "..."
-    }
-    for (var a of strategy_content_text) {
-        if (a.innerText.length > 15){
-            a.innerText = a.innerText.substring(0, 15) + "..."
+                        // 字数限制
+                        if (diary_text.innerText.length > 60) {
+                            diary_text.innerText = diary_text.innerText.substring(0, 60) + "..."
+                        }
+                        if (diary_title.innerText.length > 15) {
+                            diary_title.innerText = diary_title.innerText.substring(0, 15) + "..."
+                        }
+                    }else {
+                        alert(res["status_text"])
+                    }
+
+                })
+            }
+            getDiaryData(first_diary_id);
+            for (var user_item of user_items){
+                user_item.onclick=function () {
+                    var diary_id=this.id;
+                    getDiaryData(diary_id)
+                }
+            }
+        }else if(res['status_code']==='10008') {
+            alert(res["status_text"])
+        }
+    });
+
+    var more_diary=document.querySelector(".more_diary a");
+    more_diary.onclick=function () {
+        location.href="./pages/strategy_list.html?more_diary=true"
+    };
+
+    // 攻略
+    var strategy_content_text=document.querySelector(".strategy_content_text");
+    var diary_content_text=document.querySelector(".diary_content_text");
+
+    getData('http://127.0.0.1:8080/api/strategy/strategyTitle/',null,null,function (res) {
+        if(res && res['status_code']==='10009'){
+            for (var r of res["content"]){
+                strategy_content_text.innerHTML+=`<a href="###" class="strategy_txt" id="${r["strategy_id"]}">${r["strategy_title"]}</a>`
+            }
+            var strategy_txts=document.querySelectorAll(".strategy_txt");
+            for (var strategy_txt of strategy_txts){
+                strategy_txt.onclick=function () {
+                    location.href="./pages/strategy_info.html?strategy_id="+this.id;
+                }
+            }
+            xzzs();
+        }else{
+            alert(res["status_text"])
+        }
+    });
+
+    getData('http://127.0.0.1:8080/api/diary/diaryTitle/',null,null,function (res) {
+        if(res && res['status_code']==='10009'){
+            for (var r of res["content"]){
+                diary_content_text.innerHTML+=`<a href="#" class="diary_txt" id="${r["diary_id"]}">${r["diary_title"]}</a>`
+            }
+            var diary_txts=document.querySelectorAll(".diary_txt");
+            for (var diary_txt of diary_txts){
+                diary_txt.onclick=function () {
+                    location.href="./pages/diary_info.html?diary_id="+this.id;
+                }
+            }
+            xzzs();
+        }else{
+            alert(res["status_text"])
+        }
+    });
+
+
+    // 限制字数
+    function xzzs() {
+        //字数限制{
+        var content_text=document.querySelectorAll(".content_text a");
+        for (var a of content_text) {
+            if (a.innerText.length > 13){
+                a.innerText = a.innerText.substring(0, 13) + "..."
+            }
         }
     }
-
+    xzzs();
 
 })();
