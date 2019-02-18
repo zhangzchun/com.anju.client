b2e0f8854053205191ad380dc14d3bf17778726d
mt12321 <mtfly1@163.com>
2019-02-18 09:17:41 +0800

    添加了7张案例详情的图片,替换了3张设计师较长的图片,替换了pages里面的 case_list.html和case_detail.html,替换了js里面的case_list.js和case_detail.js

--- js/case_detail.js
+++ js/case_detail.js
@@ -1,4 +1,65 @@
 (function () {
+    //后台渲染数据
+    var content=document.querySelector('.content');
+    var case_name=document.querySelectorAll('.case_name');
+    var designer_img=document.querySelector('.designer_img');
+    var designer_name=document.querySelectorAll('.designer_name');
+    var case_num=document.querySelector('.case_num');
+    var company_icon=document.querySelector('#first');
+    var type=document.querySelector('.type');
+    var style=document.querySelector('.style');
+    var area=document.querySelector('.area');
+    var price=document.querySelector('.price');
+    var renovation=document.querySelector('.renovation');
+    var village=document.querySelector('.village');
+    var date=document.querySelector('.date');
+    var id={'case_id':location.href.split('=')[1]};
+    getData('http://127.0.0.1:8080/api/case/caseDetail/',id,null,function (res) {
+        if(res && res['status_code']==='10009'){
+            case_name[0].innerText=res['content'][0]['name'];
+            case_name[1].innerText=res['content'][0]['name'];
+            designer_img.src=res['content'][0]['icon'];
+            designer_name[0].innerText=res['content'][0]['des'];
+            designer_name[1].innerText=res['content'][0]['des'];
+            case_num.innerText=res['content'][0]['case_num'];
+            company_icon.src=res['content'][0]['company_icon'];
+            type.innerText=res['content'][0]['type'];
+            style.innerText=res['content'][0]['sty'];
+            area.innerText=res['content'][0]['area'];
+            price.innerText=res['content'][0]['price'];
+            renovation.innerText=res['content'][0]['reno'];
+            village.innerText=res['content'][0]['village'];
+            date.innerText=res['content'][0]['date'];
+
+            for(var r of res['content']){
+                content.innerHTML+=`<div class="row start_img_small">
+                    <div class="col-sm-12 col-xs-12 col-md-12">
+                        <img src="${r['img_type']}"/>
+                    </div>
+                </div>
+                <div class="row start_img">
+                    <div class="col-sm-12 col-xs-12 col-md-12">
+                        <img src="${r['img_url']}"/>
+                    </div>
+                </div>`
+            }
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //  隐藏部分
     var liu=document.querySelector('.liu');
--- js/case_list.js
+++ js/case_list.js
@@ -1,49 +1,237 @@
 (function () {
     var table=document.querySelector('.font');
     var con = document.querySelectorAll(".condition");
+    var idData=document.querySelector('#idData');
+    var id={'company_id':location.href.split('=')[1]};
+    // var id={'company_id':1};
     for (var c of con){
         c.style.display="none";
     }
+    function getCompanyData(url,args){
+        getData(url,args,null,function (res) {
+            if(res && res['status_code']==='10009'){
+                for(let i=0;i<res['content'].length;i=i+4){
+                    if(i+3<res['content'].length){
+                        idData.innerHTML+=`<tr><td><div class="row case module-cont">
+           <div class="card-bx col-md-3 col-sm-3 col-xs-6  case_item" id="${res['content'][i]['id']}">
+                <a href="#">
+                    <div class="img-ct">
+                        <img src="${res['content'][i]['img_url']}"/></div>
+                    <div class="card-info">
+                        <p class="card-name ect">${res['content'][i]['name']}</p>
+                        <p class="card-detail">${res['content'][i]['area']}m² / ${res['content'][i]['ht.name']} / ${res['content'][i]['style.name']} / ${res['content'][i]['rt.name']}</p>
+                        <p  class="card-cost ect">
+                                    <span>
+                                        ${res['content'][i]['price']}
+                                    </span>
+                            万
+                        </p>
+                    </div>
+                </a>
+            </div>
+            <div class="card-bx col-md-3 col-sm-3 col-xs-6 second_img case_item" id="${res['content'][i+1]['id']}">
+                <a href="#">
+                    <div class="img-ct">
+                        <img src="${res['content'][i+1]['img_url']}"/></div>
+                    <div class="card-info">
+                        <p class="card-name ect">${res['content'][i+1]['name']}</p>
+                        <p class="card-detail">${res['content'][i+1]['area']}m² / ${res['content'][i+1]['ht.name']} / ${res['content'][i+1]['style.name']} / ${res['content'][i+1]['rt.name']}</p>
+                        <p class="card-cost ect">
+                                    <span>
+                                        ${res['content'][i+1]['price']}
+                                    </span>
+                            万
+                        </p>
+                    </div>
+                </a>
+            </div>
+            <div class="card-bx col-md-3 col-sm-3 col-xs-6 case_item" id="${res['content'][i+2]['id']}">
+                <a href="#">
+                    <div class="img-ct">
+                        <img src="${res['content'][i+2]['img_url']}"/></div>
+                    <div class="card-info">
+                        <p class="card-name ect">${res['content'][i+2]['name']}</p>
+                        <p class="card-detail">${res['content'][i+2]['area']}m² / ${res['content'][i+2]['ht.name']} / ${res['content'][i+2]['style.name']} / ${res['content'][i+2]['rt.name']}</p>
+                        <p class="card-cost ect">
+                                    <span>
+                                        ${res['content'][i+2]['price']}
+                                    </span>
+                            万
+                        </p>
+                    </div>
+                </a>
+            </div>
+            <div class="card-bx col-md-3 col-sm-3 col-xs-6 fourth_img case_item" id="${res['content'][i+3]['id']}">
+                <a href="#">
+                    <div class="img-ct">
+                        <img src="${res['content'][i+3]['img_url']}"/></div>
+                    <div class="card-info">
+                        <p class="card-name ect">${res['content'][i+3]['name']}</p>
+                        <p class="card-detail">${res['content'][i+3]['area']}m² / ${res['content'][i+3]['ht.name']} / ${res['content'][i+3]['style.name']} / ${res['content'][i+3]['rt.name']}</p>
+                        <p class="card-cost ect">
+                                    <span>
+                                        ${res['content'][i+3]['price']}
+                                    </span>
+                            万
+                        </p>
+                    </div>
+                </a>
+            </div>
+        </div></td></tr>`
+                    }
+
+                }
+
+                //调用分页函数
+                goPage(1);
+                let case_item=document.querySelectorAll(".case_item");
+                for (var c of case_item){
+                    c.onclick=function () {
+                        location.href="case_detail.html?case_id="+this.id;
+                    }
+                }
+            }else if(res['status_code']==='10008') {
+                idData.innerHTML+=""
+            }
+
+        });
+    }
+    function postCompanyData(url,args){
+        postData(url,args,function (res) {
+            if(res && res['status_code']==='10009'){
+                for(let i=0;i<res['content'].length;i=i+4){
+                    if(i+3<res['content'].length){
+                        idData.innerHTML+=`<tr><td><div class="row case module-cont">
+           <div class="card-bx col-md-3 col-sm-3 col-xs-6  case_item" id="${res['content'][i]['id']}">
+                <a href="#">
+                    <div class="img-ct">
+                        <img src="${res['content'][i]['img_url']}"/></div>
+                    <div class="card-info">
+                        <p class="card-name ect">${res['content'][i]['name']}</p>
+                        <p class="card-detail">${res['content'][i]['area']}m² / ${res['content'][i]['ht.name']} / ${res['content'][i]['style.name']} / ${res['content'][i]['rt.name']}</p>
+                        <p  class="card-cost ect">
+                                    <span>
+                                        ${res['content'][i]['price']}
+                                    </span>
+                            万
+                        </p>
+                    </div>
+                </a>
+            </div>
+            <div class="card-bx col-md-3 col-sm-3 col-xs-6 second_img case_item" id="${res['content'][i+1]['id']}">
+                <a href="#">
+                    <div class="img-ct">
+                        <img src="${res['content'][i+1]['img_url']}"/></div>
+                    <div class="card-info">
+                        <p class="card-name ect">${res['content'][i+1]['name']}</p>
+                        <p class="card-detail">${res['content'][i+1]['area']}m² / ${res['content'][i+1]['ht.name']} / ${res['content'][i+1]['style.name']} / ${res['content'][i+1]['rt.name']}</p>
+                        <p class="card-cost ect">
+                                    <span>
+                                        ${res['content'][i+1]['price']}
+                                    </span>
+                            万
+                        </p>
+                    </div>
+                </a>
+            </div>
+            <div class="card-bx col-md-3 col-sm-3 col-xs-6 case_item" id="${res['content'][i+2]['id']}">
+                <a href="#">
+                    <div class="img-ct">
+                        <img src="${res['content'][i+2]['img_url']}"/></div>
+                    <div class="card-info">
+                        <p class="card-name ect">${res['content'][i+2]['name']}</p>
+                        <p class="card-detail">${res['content'][i+2]['area']}m² / ${res['content'][i+2]['ht.name']} / ${res['content'][i+2]['style.name']} / ${res['content'][i+2]['rt.name']}</p>
+                        <p class="card-cost ect">
+                                    <span>
+                                        ${res['content'][i+2]['price']}
+                                    </span>
+                            万
+                        </p>
+                    </div>
+                </a>
+            </div>
+            <div class="card-bx col-md-3 col-sm-3 col-xs-6 fourth_img case_item" id="${res['content'][i+3]['id']}">
+                <a href="#">
+                    <div class="img-ct">
+                        <img src="${res['content'][i+3]['img_url']}"/></div>
+                    <div class="card-info">
+                        <p class="card-name ect">${res['content'][i+3]['name']}</p>
+                        <p class="card-detail">${res['content'][i+3]['area']}m² / ${res['content'][i+3]['ht.name']} / ${res['content'][i+3]['style.name']} / ${res['content'][i+3]['rt.name']}</p>
+                        <p class="card-cost ect">
+                                    <span>
+                                        ${res['content'][i+3]['price']}
+                                    </span>
+                            万
+                        </p>
+                    </div>
+                </a>
+            </div>
+        </div></td></tr>`
+                    }
+
+                }
+                goPage(1);
+                let case_item=document.querySelectorAll(".case_item");
+                for (var c of case_item){
+                    c.onclick=function () {
+                        location.href="case_detail.html?case_id="+this.id;
+                    }
+                }
+            }else {
+                idData.innerHTML+=""
+            }
+        });
+    }
+
+
+
     table.onclick=function (event) {
         var et=event.target;
         if(et.className==='zx' || et.className==='condition' || et.className==='ago'){
             if(et.className==='condition'){
                 et.style.display='none'
+                et.innerText='';
             }
         }
         else {
             if(et.nodeName==='TD' && et.className!=='line'){
                 for(var i=1;i<et.parentNode.children.length;i++){
-                    et.parentNode.children[i].style.background='#ffffff';
+                    // et.parentNode.children[i].style.background='#ffffff';
                     if(et.parentNode.children[0].innerText==="户型"){
                         con[0].style.display="inline";
-                        con[0].innerText = et.children[0].innerText+" ×";
+                        con[0].innerText = et.children[0].innerText;
                     }else if(et.parentNode.children[0].innerText==="风格"){
                         con[1].style.display="inline";
-                        con[1].innerText = et.children[0].innerText+" ×";
+                        con[1].innerText = et.children[0].innerText;
                     }else if(et.parentNode.children[0].innerText==="面积"){
                         con[2].style.display="inline";
-                        con[2].innerText = et.children[0].innerText+" ×";
+                        con[2].innerText = et.children[0].innerText;
                     }
                 }
-                et.style.background='#f8f8f8';
+                // et.style.background='#f8f8f8';
             }else if(et.nodeName==='A'){
                 for(var i=1;i<et.parentNode.parentNode.children.length;i++){
-                    et.parentNode.parentNode.children[i].style.background='#ffffff'
+                    // et.parentNode.parentNode.children[i].style.background='#ffffff'
                     if(et.parentNode.parentNode.children[0].innerText==="户型"){
                         con[0].style.display="inline";
-                        con[0].innerText = et.innerText+" ×";
+                        con[0].innerText = et.innerText;
                     }else if(et.parentNode.parentNode.children[0].innerText==="风格"){
                         con[1].style.display="inline";
-                        con[1].innerText = et.innerText+" ×";
+                        con[1].innerText = et.innerText;
                     }else if(et.parentNode.parentNode.children[0].innerText==="面积"){
                         con[2].style.display="inline";
-                        con[2].innerText = et.innerText+" ×";
+                        con[2].innerText = et.innerText;
                     }
                 }
-                et.parentNode.style.background='#f8f8f8';
+                // et.parentNode.style.background='#f8f8f8';
             }
         }
+        if(con[0].innerText==='' && con[1].innerText==='' && con[2].innerText==='') {
+            getCompanyData('http://127.0.0.1:8080/api/case/caseList/',id)
+        };
+        idData.innerHTML="";
+        let condition={"ht_name":con[0].innerText,"style_name":con[1].innerText,"area":con[2].innerText};
+        postCompanyData('http://127.0.0.1:8080/api/case/caseScreen/',condition)
 
     };
     
@@ -87,75 +275,7 @@
 
 
 //    动态生成列表
-    var idData=document.querySelector('#idData');
-    for (var i=0;i<8;i++){
-        idData.innerHTML+=`<tr><td><div class="row case module-cont">
-            <div class="card-bx col-md-3 col-sm-3 col-xs-6">
-                <a href="#">
-                    <div class="img-ct">
-                        <img src="../image/company_img01.jpg"/></div>
-                    <div class="card-info">
-                        <p class="card-name ect">雅私阁装饰</p>
-                        <p class="card-detail">333m² / 公寓 / 其他 / 全包</p>
-                        <p class="card-cost ect">
-                                    <span>
-                                        11
-                                    </span>
-                            万
-                        </p>
-                    </div>
-                </a>
-            </div>
-            <div class="card-bx col-md-3 col-sm-3 col-xs-6 second_img">
-                <a href="#">
-                    <div class="img-ct">
-                        <img src="../image/company_img01.jpg"/></div>
-                    <div class="card-info">
-                        <p class="card-name ect">雅私阁装饰</p>
-                        <p class="card-detail">333m² / 公寓 / 其他 / 全包</p>
-                        <p class="card-cost ect">
-                                    <span>
-                                        11
-                                    </span>
-                            万
-                        </p>
-                    </div>
-                </a>
-            </div>
-            <div class="card-bx col-md-3 col-sm-3 col-xs-6">
-                <a href="#">
-                    <div class="img-ct">
-                        <img src="../image/company_img01.jpg"/></div>
-                    <div class="card-info">
-                        <p class="card-name ect">雅私阁装饰</p>
-                        <p class="card-detail">333m² / 公寓 / 其他 / 全包</p>
-                        <p class="card-cost ect">
-                                    <span>
-                                        11
-                                    </span>
-                            万
-                        </p>
-                    </div>
-                </a>
-            </div>
-            <div class="card-bx col-md-3 col-sm-3 col-xs-6 fourth_img">
-                <a href="#">
-                    <div class="img-ct">
-                        <img src="../image/company_img01.jpg"/></div>
-                    <div class="card-info">
-                        <p class="card-name ect">雅私阁装饰</p>
-                        <p class="card-detail">333m² / 公寓 / 其他 / 全包</p>
-                        <p class="card-cost ect">
-                                    <span>
-                                        11
-                                    </span>
-                            万
-                        </p>
-                    </div>
-                </a>
-            </div>
-        </div></td></tr>`
-    }
+    getCompanyData('http://127.0.0.1:8080/api/case/caseList/',id)
 
 
 })();
@@ -166,7 +286,7 @@ function goPage(pno) {
     var num = itable.rows.length;//表格所有行数(所有记录数)
     console.log(num);
     var totalPage = 0;//总页数
-    var pageSize = 3;//每页显示行数
+    var pageSize = 5;//每页显示行数
     //总共分几页
     if (num / pageSize > parseInt(num / pageSize)) {
         totalPage = parseInt(num / pageSize) + 1;
@@ -213,7 +333,7 @@ function goPage(pno) {
     }
     document.querySelector("#barcon").innerHTML = tempStr;
 }
-goPage(1)
+
 
 
 //    分页结束
